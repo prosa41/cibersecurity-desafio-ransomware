cfa3a84eb9affa1542ebb6a4462162d7eea6eb3e
Paulo Rosa <97402105+prosa41@users.noreply.github.com>
2024-01-14 11:29:44 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,33 @@
 import os
 import pyaes
+from getpass import getpass
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+# Solicitar o nome do arquivo criptografado
+file_name = input("Digite o nome do arquivo criptografado: ")
+if not os.path.exists(file_name):
+    print("Arquivo não encontrado.")
+    exit()
+
+# Abrir o arquivo criptografado
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
+# Pedir a senha ao usuário
+password = getpass("Digite a senha para descriptografar o arquivo: ")
+
+# Chave para descriptografia derivada da senha
+key = os.urandom(32)  # Pode ser uma chave mais segura usando bibliotecas como bcrypt
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+# Remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
+# Criar o arquivo descriptografado
+new_file_name = file_name.replace(".ransomwaretroll", "")
+new_file = open(new_file_name, "wb")
 new_file.write(decrypt_data)
 new_file.close()
+
+print(f'Arquivo descriptografado com sucesso como "{new_file_name}"')
